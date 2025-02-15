b7da6ff11569a8caa5d1bcb2b26e0b46e991b6bb
cetijunior <lameceti1@gmail.com>
2025-02-15 02:59:01 +0100

    Landing || Projects || Contact || Career

--- src/components/Landing/ServicesPreview.jsx
+++ src/components/Landing/ServicesPreview.jsx
@@ -62,7 +62,7 @@ const ServicesAndTechStack = () => {
                         initial={{ opacity: 0, y: 20 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.6 }}
-                        className="text-5xl font-bold text-white text-center mb-16"
+                        className="md:text-5xl text-3xl font-bold text-white text-center mb-16"
                     >
                         <div className="flex items-center justify-center ">
                             Our Premium
